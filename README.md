e6ff5e51639dd992015ee312dd21a16d08bd34bb
chanchalganvir <71035443+chanchalganvir@users.noreply.github.com>
2023-07-20 17:09:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Chat-App-OpenAssistant-API
+The GUI of Codex Leicester is based on streamlit....
+
+## Clone the Repo:
+Clone the repository. 
+```shell
+git clone https://github.com/chanchalganvir/Codex_Leicester-
+```
+
+## Environment Setup
+In order to set your environment up to run the code here, first install all requirements:
+
+```shell
+pip install -r requirements.txt
+```
+
+## OpenAI API Key 
+
+You will need the OpenAI API key to run this, get your HuggingFace key from [here](https://huggingface.co/settings/tokens)
+In the `.env` set your API key. 
+
+```shell
+HUGGINGFACEHUB_API_TOKEN=
+```
+
+## Run the WebApp:
+
+```shell
+streamlit run ownChat.py
+```
---
+++ ownChat.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+from dotenv import load_dotenv
+
+# load the Environment Variables. 
+load_dotenv()
+st.set_page_config(page_title="OpenAssistant Powered Chat App")
+
+# Sidebar contents
+with st.sidebar:
+    st.title('Your personal AI')
+    st.markdown('''
+    
+    ## Be educated, be organised, and be agitated
+    
+    ''')
+    
+    
+    add_vertical_space(3)
+    st.markdown('<p style="font-family:monospace; color:darkgreen;">Made by Chanchal C. Ganvir</p>', unsafe_allow_html=True)
+
+st.markdown('<p style="font-family:larg-cursive;font-size:70px; color:RYB Green;">Codex Leicester</p>', unsafe_allow_html=True)
+
+
+def main():
+
+    # Generate empty lists for generated and user.
+    ## Assistant Response
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hey there, great to meet you. I’m Codex Leicester, your personal AI. My goal is to be useful, friendly and providing information. Ask me for advice, for answers, or let’s talk about whatever’s on your mind. "]
+
+    ## user question
+    if 'user' not in st.session_state:
+        st.session_state['user'] = ['Hi!']
+
+    # Layout of input/response containers
+    response_container = st.container()
+    colored_header(label='', description='', color_name='blue-70')
+    input_container = st.container()
+
+    # get user input
+    def get_text():
+        input_text = st.text_input("You: ", "", key="input")
+        return input_text
+
+    ## Applying the user input box
+    with input_container:
+        user_input = get_text()
+
+    def chain_setup():
+
+
+        template = """<|prompter|>{question}<|endoftext|>
+        <|assistant|>"""
+        
+        prompt = PromptTemplate(template=template, input_variables=["question"])
+
+        llm=HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"max_new_tokens":1200})
+
+        llm_chain=LLMChain(
+            llm=llm,
+            prompt=prompt
+        )
+        return llm_chain
+
+
+    # generate response
+    def generate_response(question, llm_chain):
+        response = llm_chain.run(question)
+        return response
+
+    ## load LLM
+    llm_chain = chain_setup()
+
+    # main loop
+    with response_container:
+        if user_input:
+            response = generate_response(user_input, llm_chain)
+            st.session_state.user.append(user_input)
+            st.session_state.generated.append(response)
+            
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['user'][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
+
+if __name__ == '__main__':
+    main()
