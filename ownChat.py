4709ba790ae0a311d7ff6e737f50eb337a2f1962
chanchalganvir <71035443+chanchalganvir@users.noreply.github.com>
2024-02-19 18:46:53 +0530

    Update ownChat.py

--- ownChat.py
+++ ownChat.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
@@ -7,7 +6,7 @@ from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from dotenv import load_dotenv
 
 
-)
+
 
 
 # load the Environment Variables. 
@@ -19,31 +18,23 @@ with st.sidebar:
     st.title('Your personal AI')
     st.markdown('''
 
-     
-        
-   
-
-
+    
    ## Be educated, be organised, and be agitated
 - [LAION-AI](https://laion.ai/)
     The LLM for Codex Leicester is trained using LAION-AI.
     
     ''')
 
-   
 
 
-    
+
+
     add_vertical_space(3)
     st.markdown('<p style="font-family:monospace; color: Red;">Made by Chanchal C. Ganvir</p>', unsafe_allow_html=True)
 
 
 st.markdown('<p style="font-family:larg-cursive;font-size:40px; color:Green;text-shadow: 14 14 20px black;">Codex Leicester</p>', unsafe_allow_html=True)
 
-
-
-            
-        
 
 def main():
 
@@ -76,8 +67,8 @@ def main():
         template = """<|prompter|>{question}<|endoftext|>
         <|assistant|>"""
 
-     
-        
+
+
         prompt = PromptTemplate(template=template, input_variables=["question"])
 
         llm=HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"max_new_tokens":1200})
@@ -103,7 +94,7 @@ def main():
             response = generate_response(user_input, llm_chain)
             st.session_state.user.append(user_input)
             st.session_state.generated.append(response)
-            
+
         if st.session_state['generated']:
             for i in range(len(st.session_state['generated'])):
                 message(st.session_state['user'][i], is_user=True, key=str(i) + '_user')
